fc2a1d113b070498b7f5cc1c902758faa09143fb
momd2329jn-ux <momd2329jn@gmail.com>
2026-09-18 20:47:23 +0300

    Update CACHE_NAME to version 20

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='shamal-wasit-v11';
+const CACHE_NAME='shamal-wasit-v20';
 const APP_SHELL=['./','./index.html','./css/style.css','./js/app.js','./js/feeds.js','./images/logo.png','./images/hero.jpg','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
